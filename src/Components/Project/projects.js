3adef1f0911da734f06b85f886915f66985e02c0
dylexBnejis <benjichukwudile@gmail.com>
2023-01-12 12:45:59 +0100

    update

--- src/Components/Project/projects.js
+++ src/Components/Project/projects.js
@@ -1,5 +1,4 @@
 import slider1 from '../../Images/forecast_mockup.png'
-import slider from  '../../Images/Screenshot 71528.png'
 
 export const apps =  [
     {id:1,title:'forecastApp', src: slider1,
@@ -7,7 +6,7 @@ export const apps =  [
     technologies:'API, react.js, day.js, styled-components, cloudflare pages',
     webLink: 'https://forecastapp.pages.dev',github:'https://github.com/dylexBenjis/weatherApp'},
 
-    {id:1,title:'solarEnergy-Estimator', src: slider1,
+    {id:2,title:'solarEnergy-Estimator', src: slider1,
     descc:'This is a single page, solar energy estimator, web application. It estimates the time for charge and discharge, number of solar-panels, and batteries you need for your solar electricity set-up (not taking into account geographical solar radiance). Every calculation is done on the client-side. This makes the webpage very fast and responsive. ', 
     technologies:'react.js, styled-components, cloudflare pages',
     webLink: 'https://forecastapp.pages.dev',github:'https://github.com/dylexBenjis/weatherApp'},
